248a3c5e6b8b6b854a8f554713b8ee70af1ca096
Lony jock <lonyjock5555@gmail.com>
2026-09-01 09:50:33 +0300

    Add files via upload

---
+++ all_about_me.py
@@ -0,0 +1,12 @@
+# This program is introduce me and my goal
+
+name = ("Lony Jock")
+age = ("22")
+favorite_language = ("python")
+goal = ("to become a skilled software developer")
+
+print("My name is", name)
+print("I am", age, "years old.")
+print("My favourite programming language is", favorite_language)
+print("My goal for this course is", goal)
+
